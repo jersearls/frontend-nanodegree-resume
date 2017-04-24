bb805592e41c8440f82d2dd4072dab7cb5c15011
Jeremy <searls@me.com>
2017-04-23 22:42:49 -0400

    append fun

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,6 @@
-$("#main").append("Jeremy Searls is a really neat fellow");
+//$("#main").append("Jeremy Searls is a really neat fellow ");
 var awesome = "I am AWESOME!";
 var fun = awesome.replace("AWESOME", "FUN");
 console.log(awesome);
 console.log(fun);
 $("#main").append(fun);
-
