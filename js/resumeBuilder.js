97fcfbd0e8487437827e899d6c82ebd93cd4ab33
Jeremy <searls@me.com>
2017-04-24 11:56:15 -0400

    create name and role variables, prepend to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,16 @@
 //$("#main").append("Jeremy Searls is a really neat fellow ");
-var awesome = "I am AWESOME!";
-var fun = awesome.replace("AWESOME", "FUN");
-console.log(awesome);
-console.log(fun);
-$("#main").append(fun);
+//var awesome = "I am AWESOME!";
+//var fun = awesome.replace("AWESOME", "FUN");
+//console.log(awesome);
+//console.log(fun);
+//$("#main").append(fun);
+
+var myName = "Jeremy Searls"
+var formattedName = HTMLheaderName.replace("%data%", myName);
+
+var myRole = "Technical Consultant"
+var formattedRole = HTMLheaderRole.replace("%data%", myRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
